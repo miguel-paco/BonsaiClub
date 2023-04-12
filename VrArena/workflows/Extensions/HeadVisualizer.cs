c4320ffad0a93d5af834127ea33bbd5764105e94
miguel-paco <miguel.paco@neuro.fchampalimaud.org>
2023-04-12 17:02:32 +0100

    General Cleaning CheckPoint 1
    - Making the Time counts more accurate.
    - Cleaning the Workflow

--- VrArena/workflows/Extensions/HeadVisualizer.cs
+++ VrArena/workflows/Extensions/HeadVisualizer.cs
@@ -11,13 +11,13 @@ using OpenCV.Net;
 [WorkflowElementCategory(ElementCategory.Transform)]
 public class HeadVisualizer
 {
-    public IObservable<IplImage> Process(IObservable<Tuple<IplImage, Tuple<Point2f,Point2f>>> source)
+    public IObservable<IplImage> Process(IObservable<Tuple<IplImage, Point2f,Point2f>> source)
     {
         return source.Select(value => 
         { 
             var image = value.Item1.Clone();
-            var centroid = new Point((int)value.Item2.Item1.X,(int)value.Item2.Item1.Y);
-            var head = new Point((int)value.Item2.Item2.X,(int)value.Item2.Item2.Y);
+            var centroid = new Point((int)value.Item2.X,(int)value.Item2.Y);
+            var head = new Point((int)value.Item3.X,(int)value.Item3.Y);
             CV.Circle(image, centroid, 1, Scalar.Rgb(0, 0, 0), -1);
             CV.Circle(image, head, 1, Scalar.Rgb(255, 0, 0), -1);
             return image;
